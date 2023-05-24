b7b7d29b39de64b42d484d33d52d6692a467ad94
gregp121 <gregoryp121@gmail.com>
2023-05-24 16:04:01 -0400

    Fix: Adding new tests

--- base_test.py
+++ base_test.py
@@ -7,5 +7,15 @@ print("Hello Testing!")
 def test_pass(): # Had to create subclasses for functions
     assert True # Had to use selfassert
 
-def test_fail():
-    assert False
+def test_uppercase():
+    assert "loud noises".upper() == "LOUD NOISES"
+
+def test_reversed():
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+def test_some_primes():
+    assert 37 in {
+        num
+        for num in range(2, 50)
+        if not any(num % div == 0 for div in range(2, num))
+    }
