795e4aeb311fa14d0c9fcf31f3a232edf904532b
gregp121 <gregoryp121@gmail.com>
2023-05-24 11:46:13 -0400

    Update base_test.py
    Fix: Removing deprecated code

--- base_test.py
+++ base_test.py
@@ -2,8 +2,8 @@
 
 # Unit tests supports simple discovery. To be found, all test files must be in modules or packages importable from the top-level
 
-from unittest import TestCase
-class pyTesting(testCase):
+class pyTesting():
+    print("Hello world!")
     def test_pass(self): # Had to create subclasses for functions
         self.assertTrue(True) # Had to use selfassert
 
