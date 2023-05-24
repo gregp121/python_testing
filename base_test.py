3337b637b3b3bf014b79ccff28e9a9c720e1889b
gregp121 <gregoryp121@gmail.com>
2023-05-24 13:39:15 -0400

    Feat: Adding PyTest back in

--- base_test.py
+++ base_test.py
@@ -3,10 +3,9 @@
 # Unit tests supports simple discovery. 
 # To be found,  test files must be importable from the top-level
 
-class pyTesting():
-    print("Hello world!")
-    def test_pass(self): # Had to create subclasses for functions
-        self.assertTrue(True) # Had to use selfassert
+print("Hello Testing!")
+def test_pass(self): # Had to create subclasses for functions
+    assert True # Had to use selfassert
 
-    def test_fail(self):
-        self.assertTrue(False)
+def test_fail(self):
+    assert False
