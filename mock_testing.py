5bff4cec1b66ea7dfd246e037cfda5d0dfef0ab3
gregp121 <gregoryp121@gmail.com>
2023-05-26 10:03:53 -0400

    Feat: Object Mocking
    Added tests for object mocking.

--- mock_testing.py
+++ mock_testing.py
@@ -1,3 +1,10 @@
+import time
+
 class Calculator:
     def sum(a, b):
         return a + b
+    
+def returnVal():
+    from constants import CONSTANT_VAL
+    time.sleep(1000)
+    return CONSTANT_VAL
--- test_2.py
+++ test_2.py
@@ -1,3 +1,14 @@
-import mock_testing
+import mock_testing #Our test target
 
-assert mock_testing.Calculator.sum(1, 2) == 3
+def testCal():
+    assert mock_testing.Calculator.sum(1, 2) == 3
+
+def testVal(mocker):
+    # returnVal has a wait, we do not want to slow down our tests
+    # Break out the API (integration) and value tests
+    # MagicMock - a placeholder
+        # Mock a constant, object, function
+        # You want to mokc where the object is import INTO not FROM
+        # So if you already import B into A, you patch in A
+    mocker.patch.object(mock_testing.returnVal, 'CONSTANT_VAL', 400) # Patching where imported. Value is usually 200
+    assert mock_testing.returnVal() == 200
