76550cb3c1083b645fea2bdc7ba2cf3af2ae53a8
gregp121 <gregoryp121@gmail.com>
2023-05-26 10:16:16 -0400

    Fix: Mocking
    Fix imports for mocking

--- mock_testing.py
+++ mock_testing.py
@@ -1,10 +1,10 @@
 import time
+from constants import CONSTANT_VAL
 
 class Calculator:
     def sum(a, b):
         return a + b
     
 def returnVal():
-    from constants import CONSTANT_VAL
     time.sleep(1000)
     return CONSTANT_VAL
--- test_2.py
+++ test_2.py
@@ -1,14 +1,16 @@
 import mock_testing #Our test target
+from mock_testing import returnVal
 
 def testCal():
     assert mock_testing.Calculator.sum(1, 2) == 3
 
 def testVal(mocker):
-    # returnVal has a wait, we do not want to slow down our tests
-    # Break out the API (integration) and value tests
-    # MagicMock - a placeholder
-        # Mock a constant, object, function
-        # You want to mokc where the object is import INTO not FROM
-        # So if you already import B into A, you patch in A
-    mocker.patch.object(mock_testing.returnVal, 'CONSTANT_VAL', 400)
-    assert mock_testing.returnVal() == 200
+    mocker.patch.object(mock_testing, 'CONSTANT_VAL', 400)
+    assert returnVal() == 200
+
+# returnVal has a wait, we do not want to slow down our tests
+# Break out the API (integration) and value tests
+# MagicMock - a placeholder
+    # Mock a constant, object, function
+    # You want to mokc where the object is import INTO not FROM
+    # So if you already import B into A, you patch in A
