c16500914f171219740475d572e924bfc670643d
gregp121 <gregoryp121@gmail.com>
2023-05-26 11:55:32 -0400

    Feat: Update mocking

--- mock_testing.py
+++ mock_testing.py
@@ -4,7 +4,11 @@ from constants import CONSTANT_VAL
 class Calculator:
     def sum(a, b):
         return a + b
-    
+
+def valCalc():
+    return CONSTANT_VAL
+
 def returnVal():
     # time.sleep(1000)
-    return CONSTANT_VAL
+    RETURN_VAL = valCalc()
+    return RETURN_VAL
