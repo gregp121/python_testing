8ec5020ddaf80482942fec9126d9d47b9dc6f9b8
gregp121 <gregoryp121@gmail.com>
2023-06-12 16:18:44 -0400

    Fix: Run user migrations

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     }
 }
 
+# User Validation - Add to base settings
+AUTH_USER_MODEL = 'authentication.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
