bd729f6776974d566a33927d43407a561e77e01f
gregp121 <gregoryp121@gmail.com>
2023-06-12 16:51:57 -0400

    Fix: Add login page

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+import authentication.views
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path('', authentication.views.login_page, name='login'),
 ]
